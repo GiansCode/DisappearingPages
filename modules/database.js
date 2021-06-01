03a406e7f2f03fa11810235670aac460ad81a4fe
pioupia <49518790+pioupia@users.noreply.github.com>
2021-06-01 11:43:02 +0200

    Update database.js

--- modules/database.js
+++ modules/database.js
@@ -2,21 +2,8 @@ const sql = require("sqlite");
 sql.open("src/database.sqlite");
 
 module.exports = class DatabaseHandler {
-    db(string, method) {
+    db(string, array = [], method = 'get') {
         return new Promise((resolve, reject) => {
-            method ? method : "get";
-            sql[method](string).then(row => {
-                if (!row) resolve("no-data");
-                resolve(row);
-            }).catch(err => {
-                resolve("no-data");
-            })
-        });
-    }
-
-    piwa(string, array, method) {
-        return new Promise((resolve, reject) => {
-            method ? method : "get";
             sql[method](string, array).then(row => {
                 if (!row) resolve("no-data");
                 resolve(row);
@@ -25,10 +12,9 @@ module.exports = class DatabaseHandler {
             })
         });
     }
-
     async checkWebsite(db) {
-        const d = new Date().getTime();
-        db(`DELETE FROM db WHERE date <= "${d}"`, "run");
+        const d = Date.now();
+        db(`DELETE FROM db WHERE date <= "${d}"`, [],"run");
     }
 
 }
